15861bc3664b78c669398b10532de8bb85a825cf
jwschmutz <jeremiah.schmutz@gmail.com>
2023-07-04 03:25:02 -0400

    port clg

--- server.js
+++ server.js
@@ -56,4 +56,4 @@ app.get("/events", async (req, res) => {
     });
 });
 
-app.listen(port, () => console.log("Server started on port 3001"));
+app.listen(port, () => console.log("Server started on port " + port));
