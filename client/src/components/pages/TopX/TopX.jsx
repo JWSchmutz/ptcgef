15b4c486141418cc4d5244dbbdb1f0ec0a6e927e
jwschmutz <jeremiah.schmutz@gmail.com>
2024-10-03 02:07:31 -0400

    added top x race beta

--- client/src/components/pages/TopX/TopX.jsx
+++ client/src/components/pages/TopX/TopX.jsx
@@ -1,35 +1,40 @@
 import { useState, useEffect } from "react";
 import "./TopX.css";
-import pokemon from "../../../data/pokemon";
+import america from "../../../data/america";
 
 function TopX() {
-  useEffect(() => {
-    const options = {
-      method: "POST",
-      headers: {
-        accept: "application/json",
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        APIKEY: "supercalifragilisticexpialidocious",
-        players: [
-          {
-            name: "Seag*",
-            game: "tcg",
-            division: "master",
-            country: "*",
-          },
-        ],
-      }),
-    };
-    fetch("https://pokedata.ovh/2024/api/", options).then((response) =>
-      response.json().then((data) => console.log(data))
-    );
-  }, []);
-
   return (
     <main id="top-x">
-      <h2 className="page-title">Top X Race</h2>
+      <h2 className="page-title">Top X Race - (NA Masters Beta)</h2>
+      <h3>CP race excluding locals</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Placement</th>
+            <th>Name</th>
+            <th>Majors CP</th>
+            <th>CP Finishes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(america)
+            .sort(
+              (a, b) =>
+                america[b].reduce((total, num) => total + num, 0) -
+                america[a].reduce((total, num) => total + num, 0)
+            )
+            .map((keyName, i) => (
+              <tr className="travelcompany-input" key={i}>
+                <td> {i + 1}</td>
+                <td>{keyName}</td>
+                <td>
+                  {america[keyName].reduce((total, num) => total + num, 0)}
+                </td>
+                <td>{america[keyName].join(", ")}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </main>
   );
 }
