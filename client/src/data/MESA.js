77b8f9895ed89e39aac7414a9dd0e507f43c7d67
jwschmutz <jeremiah.schmutz@gmail.com>
2024-11-18 15:12:25 -0500

    laic, buenos aires pre accurate

--- client/src/data/MESA.js
+++ client/src/data/MESA.js
@@ -4,6 +4,9 @@ const MESAFinishes = {
   "David Kramer [IL]": [100],
   "Amir Oppenheim-Nevo [IL]": [80],
   "Raz Shamir [IL]": [80],
+  "Michael Jordan [ZA]": [80],
+  "Raz Wolpe [IL]": [380],
+  "Jateen Algu [ZA]": [80],
 };
 
 export default MESAFinishes;
--- client/src/data/OC.js
+++ client/src/data/OC.js
@@ -1,7 +1,12 @@
 const OCFinishes = {
-  "Brent Tonisson [AU]": [125, 125, 100, 40],
-  "Kaiwen Cabbabe [AU]": [280, 125],
+  "Brent Tonisson [AU]": [480, 125, 125, 100, 40],
+  "Kaiwen Cabbabe [AU]": [280, 125, 120, 80],
   "Ryan Bielak [AU]": [40],
+  "Tim Franklin [AU]": [280, 200],
+  "Matthew Burris [AU]": [200],
+  "Vignesh Krishnan [AU]": [120],
+  "NATALIE Millar [AU]": [100],
+  "Ciaran Bradley [NZ]": [80],
 };
 
 export default OCFinishes;
